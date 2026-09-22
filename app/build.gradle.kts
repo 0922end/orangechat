4c473ff771a319786b098ec5d9068956ed2919b3
0922end <1828183920@qq.com>
2026-09-22 10:29:47 +0800

    视频通话: 加camera-view依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -299,6 +299,7 @@ dependencies {
     implementation(libs.androidx.camera.core)
     implementation(libs.androidx.camera.camera2)
     implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
 
     // Room
     implementation(libs.androidx.room.runtime)
